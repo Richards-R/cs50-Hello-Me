30cd81eaac2d06be35a43883bba7608869126d97
Richards-R <130212450+Richards-R@users.noreply.github.com>
2024-01-07 01:09:23 +0900

    amend style

--- me/hello.c
+++ me/hello.c
@@ -1,11 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 
-    string name_input = get_string ("What's your name? ");
+    string name_input = get_string("What's your name? ");
 
     printf("Hello, %s\n", name_input);
-
 }
